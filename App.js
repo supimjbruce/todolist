b8792e3470ab0c380d403a752ecfc69b50a3a803
Jonathan Bruce <supimjbruce@gmail.com>
2025-03-13 22:26:29 -0400

    Started todolist
    Hello GitHub!
    
    I started work on todolist tonight, a very simple app with an MIT license to help other beginner coders like me learn the basics of code!
    
    Started to make this an Expo React Native project but Expo Go was giving me issues so I just coded very basic React Native code and called it a night.
    
    Learned CMD A + CMD + / on VSCODE so that was cool!
    
    Goodnight GitHub!
    
    .j.

---
+++ App.js
@@ -0,0 +1,14 @@
+// import React from 'React';
+// import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+
+// export default function TodoList() {
+//     return (
+//         <View> {/*I would like a simple background for this, nice font, and clean UI./*}
+//             <Text>Todo List</Text> {/*Todo List Title*/}
+//             <View> {/*Todo List Card*/}
+//                 <Text>Todo List Title</Text> {/*Todo Item Title*/}
+//                 <TouchableOpacity /> {/*Todo Item Check Button*/}
+//             </View> {/*Turn this into a component*/}
+//         </View>
+//     )
+// }
